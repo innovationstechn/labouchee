946e0655384ada14ed6d415d60a76fb7732c3e9f
Saad Mehmood <innovationstechn@gmail.com>
2022-02-19 22:38:09 +0500

    Added a responsive design for home page.

--- lib/pages/landing/home_view.dart
+++ lib/pages/landing/home_view.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:getwidget/components/card/gf_card.dart';
+import 'package:getwidget/components/carousel/gf_carousel.dart';
+import 'package:getwidget/components/carousel/gf_items_carousel.dart';
+import 'package:getwidget/components/tabs/gf_tabbar.dart';
+import 'package:getwidget/components/tabs/gf_tabbar_view.dart';
+import 'package:labouchee/models/product.dart';
 import 'package:labouchee/pages/home/category.dart';
 
 import '../home/carousel_slider.dart';
@@ -11,39 +19,303 @@ class LandingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ViewModelBuilder<LandingVM>.reactive(
-        viewModelBuilder: () => LandingVM(),
-        onModelReady: (model) => model.initialize(),
-        builder: (context, landingVM, _) {
-          if (landingVM.isBusy) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (landingVM.hasError) {
+      body: SafeArea(
+        child: ViewModelBuilder<LandingVM>.reactive(
+          viewModelBuilder: () => LandingVM(),
+          onModelReady: (model) => model.initialize(),
+          builder: (context, landingVM, _) {
+            if (landingVM.isBusy) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (landingVM.hasError) {
+              return Center(
+                child: Text(
+                  landingVM.error(landingVM),
+                ),
+              );
+            }
+
             return Center(
-              child: Text(
-                landingVM.error(landingVM),
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 800),
+                child: SingleChildScrollView(
+                  child: LayoutBuilder(builder: (context, constraints) {
+                    final int hItemCount = constraints.maxWidth.round() ~/ 150;
+
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8.0),
+                          child: GFCarousel(
+                            pagination: true,
+                            passiveIndicator: Colors.white.withAlpha(100),
+                            activeIndicator: Colors.white.withAlpha(200),
+                            viewportFraction: 1.0,
+                            autoPlay: true,
+                            enableInfiniteScroll: true,
+                            items: landingVM.banners
+                                .map(
+                                  (e) => ClipRRect(
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(5.0)),
+                                    child: Image.network(
+                                      e.photo!,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                        ),
+                        DefaultTabController(
+                          length: 3,
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 50,
+                                child: TabBar(
+                                  unselectedLabelColor: Colors.redAccent,
+                                  indicatorSize: TabBarIndicatorSize.label,
+                                  indicator: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      color: Colors.redAccent),
+                                  tabs: [
+                                    Tab(
+                                      child: buildCategoryCapsule('FEATURED'),
+                                    ),
+                                    Tab(
+                                      child: buildCategoryCapsule('HOT SALE'),
+                                    ),
+                                    Tab(
+                                      child: buildCategoryCapsule('VIEWED'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 220,
+                                child: TabBarView(children: [
+                                  buildTab(landingVM.featured, hItemCount),
+                                  buildTab(landingVM.hotSale, hItemCount),
+                                  buildTab(landingVM.mostViewed, hItemCount),
+                                ]),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              buildCategoryCapsule('OUR CATALOG'),
+                            ],
+                          ),
+                        ),
+                        LayoutBuilder(
+                          builder: (context, constraints) {
+                            return AlignedGridView.count(
+                              itemCount: landingVM.products.length,
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              itemBuilder: (BuildContext context, int index) {
+                                final item = landingVM.products[index];
+
+                                return productCard(item);
+                              },
+                              crossAxisCount:
+                                  constraints.maxWidth > 400 ? 2 : 1,
+                            );
+                          },
+                        ),
+                        // ListView.builder(
+                        //   physics: const NeverScrollableScrollPhysics(),
+                        //   shrinkWrap: true,
+                        //   itemCount: landingVM.products.length,
+                        //   itemBuilder: (context, index) {
+                        //     final item = landingVM.products[index];
+                        //
+                        //     return productCard(item);
+                        //   },
+                        // )
+                      ],
+                    );
+                  }),
+                ),
               ),
             );
-          }
+          },
+        ),
+      ),
+    );
+  }
 
-          return Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: CustomScrollView(
-              slivers: [
-                SliverList(
-                  delegate: SliverChildListDelegate([
-                    CarouselSlider(),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Category()
-                  ]),
+  Widget buildTab(List<ProductModel> products, int hItemCount) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GFItemsCarousel(
+            rowCount: hItemCount,
+            children: products.map(
+              (item) {
+                return Container(
+                  margin: const EdgeInsets.all(4),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                    child: Image.network(item.images!.first,
+                        fit: BoxFit.cover, width: 1000.0),
+                  ),
+                );
+              },
+            ).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget productCard(ProductModel item) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 300),
+      margin: const EdgeInsets.all(8),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                item.name!,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  overflow: TextOverflow.ellipsis,
                 ),
-              ],
+              ),
+              const Spacer(),
+              Text(
+                item.productRating?.toStringAsFixed(1) ?? "-",
+                style:
+                    const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.yellow[700],
+                size: 18,
+              )
+            ],
+          ),
+
+          const SizedBox(
+            height: 10,
+          ),
+          LayoutBuilder(builder: (context, constraints) {
+            return ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+              child: Image.network(
+                item.images!.first,
+                fit: BoxFit.cover,
+              ),
+            );
+          }),
+          // LayoutBuilder(
+          //   builder: (context, constraints) {
+          //     return Wrap(
+          //       crossAxisAlignment: WrapCrossAlignment.center,
+          //       alignment: WrapAlignment.center,
+          //       children: item.images!
+          //           .map(
+          //             (e) => Container(
+          //               width: constraints.maxWidth < 500 ? double.infinity : 250,
+          //               margin: const EdgeInsets.all(4),
+          //               child: ClipRRect(
+          //                 borderRadius:
+          //                     const BorderRadius.all(Radius.circular(5.0)),
+          //                 child: Image.network(
+          //                   e,
+          //                   fit: BoxFit.cover,
+          //                 ),
+          //               ),
+          //             ),
+          //           )
+          //           .toList(),
+          //     );
+          //   }
+          // ),
+
+          const SizedBox(
+            height: 10,
+          ),
+          LayoutBuilder(
+            builder: (context, constraints) {
+              return SizedBox(
+                width: constraints.maxWidth,
+                child: Row(
+                  children: [
+                    if (item.priceSmall != null) buildSizeCapsule('SMALL'),
+                    if (item.priceMedium != null) buildSizeCapsule('MEDIUM'),
+                    if (item.priceLarge != null) buildSizeCapsule('LARGE'),
+                  ],
+                ),
+              );
+            }
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            item.description!,
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
+          ),
+          // const Divider()
+        ],
+      ),
+    );
+  }
+
+  Widget buildCategoryCapsule(String name, {Color? color}) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      height: 50,
+      decoration: BoxDecoration(
+          color: color ?? Colors.redAccent,
+          borderRadius: BorderRadius.circular(50)),
+      child: Align(
+        alignment: Alignment.center,
+        child: FittedBox(
+          child: Text(
+            name,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSizeCapsule(String name) {
+    return FittedBox(
+      child: Container(
+        margin: const EdgeInsets.only(right: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        height: 25,
+        decoration: BoxDecoration(
+          color: Colors.redAccent,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: FittedBox(
+            child: Text(
+              name,
+              style: const TextStyle(color: Colors.white),
             ),
-          );
-        },
+          ),
+        ),
       ),
     );
   }
