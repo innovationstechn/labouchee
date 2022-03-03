080ff30e3bd24c38808753bfe5fd494fab50d509
Saad Mehmood <innovationstechn@gmail.com>
2022-03-04 00:50:19 +0500

    Added a parameter to product filtering for filtering categories.

--- lib/models/product_filter.dart
+++ lib/models/product_filter.dart
@@ -4,7 +4,7 @@ part 'product_filter.g.dart';
 
 @JsonSerializable(fieldRename: FieldRename.snake, includeIfNull: false)
 class ProductFilterModel {
-  final int? page, min, max;
+  final int? page, min, max, categoryId;
   final String? sort;
   @JsonKey(name: 'q')
   final String? query;
@@ -21,6 +21,7 @@ class ProductFilterModel {
     this.mostViewed,
     this.featured,
     this.hotSale,
+    this.categoryId,
   });
 
 
