10d6a8aedabea2b8f6fd18735ca1fc17b32d90d8
Saad Mehmood <innovationstechn@gmail.com>
2022-03-08 12:59:50 +0500

    Fixed broken snackbars after Router migration.

--- lib/services/navigator.dart
+++ lib/services/navigator.dart
@@ -1,9 +1,9 @@
-import 'package:labouchee/app/auth_guard.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 import '../app/routes.gr.dart' as auto_router;
 
 class NavigatorService {
-  final auto_router.Router _router = auto_router.Router();
+  final auto_router.Router _router = auto_router.Router(StackedService.navigatorKey);
 
   auto_router.Router get router => _router;
 }
